ca6df53b1390b163a2c3ef2cf2519f5d0d8f0e0d
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-09-23 14:39:39 -0300

    instalações

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+//SL - SERVER LIST
+//SN - SERVER NAME
+//CI - CHANNEL INFO
+//CL - CHANNEL LIST
+//CD - CHANNEL DATA
+//UL - USER LIST
+//UI - USER INFO
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns:  ;
+  grid-auto-rows: ;
+
+  grid-template-areas:
+  'SL SN CI CI'
+  'SL SN CD UL'
+  'SL UI CD UL'
+  
+`;
